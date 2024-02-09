b4f85ab8f892c07c7f8298d44027dfb45f53dae9
Santi <santiagofrau3@gmail.com>
2024-02-09 14:41:48 -0300

    v1.1

--- src/data/lenguajes.js
+++ src/data/lenguajes.js
@@ -1,19 +1,17 @@
 export const lenguajes_estilos = [
     {
         estilos:"shadow-o bg-orange-600",
-        img:"/img/0.webp"
+        img:"/public/img/0.webp"
     },
     {
         estilos:"bg-blue-800 shadow-b",
-        img:"/img/1.webp"
+        img:"/public/img/1.webp"
     },
     {
         estilos:"bg-amber-300 shadow-y",
-        img:"/img/2.webp"
+        img:"/public/img/2.webp"
     }
 ]
 
 
 
-//img:"/dist/img/2.webp" produccion 
-///img/2.webp desarrollo
