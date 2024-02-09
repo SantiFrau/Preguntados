5437f697d5c3310a47de2fc72d9a79b2d47df5a0
Santi <santiagofrau3@gmail.com>
2024-02-09 13:14:42 -0300

    v2.2.2

--- src/data/lenguajes.js
+++ src/data/lenguajes.js
@@ -1,14 +1,15 @@
 export const lenguajes_estilos = [
     {
         estilos:"shadow-o bg-orange-600",
-        img:"/Preguntados/public/img/0.webp"
+        img:"/Preguntados/img/0.webp"
     },
     {
         estilos:"bg-blue-800 shadow-b",
-        img:"/Preguntados/public/img/1.webp"
+        img:"/Preguntados/img/1.webp"
     },
     {
         estilos:"bg-amber-300 shadow-y",
-        img:"/Preguntados/public/img/2.webp"
+        img:"/Preguntados/img/2.webp"
     }
 ]
+
