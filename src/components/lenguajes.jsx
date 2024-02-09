b386124db2eb0e20e21e3fd52cfbca1915f775da
Santi <santiagofrau3@gmail.com>
2024-02-09 15:00:57 -0300

    v3.1

--- src/components/lenguajes.jsx
+++ src/components/lenguajes.jsx
@@ -9,9 +9,9 @@ export default function Lenguajes() {
   return (
     <section className=' h-max gap-10 w-full justify-center flex flex-row items-center bg-opacity-10 rounded-xl bg-stone-200 md:w-2/3 mt-3 p-3'>
       {
-        lenguajes_estilos.map((lenguaje)=>{
+        lenguajes_estilos.map((lenguaje,id)=>{
           return(
-            <div className={`p-3 ${lenguajes[0].estado ? lenguaje.estilo : "bg-opacity-20 bg-stone-200"} rounded-full`}>
+            <div className={`p-3 ${lenguajes[id].estado ? lenguaje.estilos : "bg-opacity-20 bg-stone-200"} rounded-full`}>
               <img className="w-10" src={`${lenguaje.img}`} alt="" />
             </div>
           )
